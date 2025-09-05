676eac359816f954e4d3010759f8bdfb6d24ee97
jay bhavani <harpalsinhdodiya08@gmail.com>
2025-09-05 04:12:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "<h1>Hello from Flask!</h1><p>This is a dynamic Python app.</p>"
 
-if _name_ == "__main__":
+if __name__ == "__main__":
 
     app.run(debug=True)
+
